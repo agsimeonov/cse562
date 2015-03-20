b346a88913ee6d8f1a782678e82fc7f62520ad45
Alexander Simeonov <agsimeon@buffalo.edu>
2015-03-19 23:10:59 -0400

    Progress towards Two-Phase, Multiway Merge-Sort.

--- src/edu/buffalo/cse562/iterator/MergeSortIterator.java
+++ src/edu/buffalo/cse562/iterator/MergeSortIterator.java
@@ -1,12 +1,16 @@
 package edu.buffalo.cse562.iterator;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import net.sf.jsqlparser.statement.select.OrderByElement;
 import edu.buffalo.cse562.table.Row;
 import edu.buffalo.cse562.table.RowComparator;
+import edu.buffalo.cse562.table.TableManager;
 
 /**
  * Two-Phase, Multiway Merge-Sort
@@ -19,7 +23,8 @@ public class MergeSortIterator implements RowIterator {
   private final RowIterator          iterator;
   private final List<OrderByElement> orderByElements;
   private ArrayList<Row>             buffer;
-  private static File                swapDirectory;
+  private ArrayList<File>            buffers;
+  private File                       swapDirectory;
 
   public MergeSortIterator(RowIterator iterator, List<OrderByElement> orderByElements) {
     this.iterator = iterator;
@@ -47,15 +52,38 @@ public class MergeSortIterator implements RowIterator {
 
   @Override
   public void open() {
-//    if (buffer != null) return;
+    if (buffers != null) return;
     iterator.open();
+    swapDirectory = new File(TableManager.getSwapDir());
+    buffers = new ArrayList<File>();
     buffer = new ArrayList<Row>();
+    
+    // Phase 1:
     while (iterator.hasNext()) {
       buffer.add(iterator.next());
-      // TODO: Figure out a way to poll for free memory less
-      if (Runtime.getRuntime().freeMemory() < THRESHOLD) {
+      
+      // When the memory is full flush the buffer onto disk
+      if (Runtime.getRuntime().freeMemory() < THRESHOLD || !iterator.hasNext()) {
+        // Sort first
         buffer.sort(new RowComparator(orderByElements));
         
+        // Now flush buffer onto disk
+        try {
+          File temporary = File.createTempFile("tmp", null, swapDirectory);
+//          temporary.deleteOnExit();
+          buffers.add(temporary);
+          
+          ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(temporary));
+          for (Row row : buffer)
+            oos.writeObject(row);
+          oos.writeObject(null);
+          oos.close();
+          
+          buffer = new ArrayList<Row>();
+          System.gc();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
