33e90ec8b3b6c496fb781947d99818142f373cb5
Alexander Simeonov <agsimeon@buffalo.edu>
2015-03-21 15:39:10 -0400

    Remoed unnecessary imports.

--- src/edu/buffalo/cse562/iterator/MergeSortIterator.java
+++ src/edu/buffalo/cse562/iterator/MergeSortIterator.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
