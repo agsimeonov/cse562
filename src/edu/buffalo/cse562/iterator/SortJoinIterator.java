ded3111c7d258e4fbd07654aebfaa9163548143b
Alexander Simeonov <agsimeon@buffalo.edu>
2015-03-22 05:00:15 -0400

    Progress towards Sort Join

--- src/edu/buffalo/cse562/iterator/SortJoinIterator.java
+++ src/edu/buffalo/cse562/iterator/SortJoinIterator.java
@@ -1,19 +1,14 @@
 package edu.buffalo.cse562.iterator;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import net.sf.jsqlparser.expression.BinaryExpression;
 import net.sf.jsqlparser.expression.DateValue;
 import net.sf.jsqlparser.expression.DoubleValue;
 import net.sf.jsqlparser.expression.LeafValue;
-import net.sf.jsqlparser.expression.LeafValue.InvalidLeaf;
 import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.StringValue;
-import net.sf.jsqlparser.schema.Column;
-import net.sf.jsqlparser.statement.select.OrderByElement;
+import net.sf.jsqlparser.expression.LeafValue.InvalidLeaf;
 import edu.buffalo.cse562.table.Row;
-import edu.buffalo.cse562.table.Schema;
 
 public class SortJoinIterator implements RowIterator {
   private static final int  NONE     = 0;
@@ -24,8 +19,8 @@ public class SortJoinIterator implements RowIterator {
   private static final int  MISMATCH = 5;
   private final RowIterator leftIterator;
   private final RowIterator rightIterator;
-  private final Integer     leftIndex;
-  private final Integer     rightIndex;
+  private final int         leftIndex;
+  private final int         rightIndex;
   private ArrayList<Row>    stash;
   private int               stashIndex;
   private int               type;
@@ -35,78 +30,32 @@ public class SortJoinIterator implements RowIterator {
 
   public SortJoinIterator(RowIterator leftIterator,
                           RowIterator rightIterator,
-                          Schema leftSchema,
-                          Schema rightSchema,
-                          BinaryExpression expression) {
-    // Acquire the columns
-    Column firstColumn = (Column) expression.getLeftExpression();
-    Column secondColumn = (Column) expression.getRightExpression();
-
-    // Initialize the order as ascending
-    OrderByElement leftOrder = new OrderByElement();
-    leftOrder.setAsc(true);
-    OrderByElement rightOrder = new OrderByElement();
-    rightOrder.setAsc(true);
-    
-    // Acquire the correct left and right columns and their index
-    Integer index = leftSchema.getIndex(firstColumn);
-    if (index == null) {
-      leftIndex = leftSchema.getIndex(secondColumn);
-      leftOrder.setExpression(secondColumn);
-      rightIndex = rightSchema.getIndex(firstColumn);
-      rightOrder.setExpression(firstColumn);
-    } else {
-      leftIndex = index;
-      leftOrder.setExpression(firstColumn);
-      rightIndex = rightSchema.getIndex(secondColumn);
-      rightOrder.setExpression(secondColumn);
-    }
-    
-    // Sort the left and right inputs TODO: Move this out of here and into the node
-    List<OrderByElement> orders = new ArrayList<OrderByElement>(1);
-    orders.add(leftOrder);
-    this.leftIterator = new MergeSortIterator(leftIterator, orders, leftSchema);
-    orders = new ArrayList<OrderByElement>(1);
-    orders.add(rightOrder);
-    this.rightIterator = new MergeSortIterator(rightIterator, orders, rightSchema);
-    
-    type = NONE;
+                          int leftIndex,
+                          int rightIndex) {
+    this.leftIterator = leftIterator;
+    this.rightIterator = rightIterator;
+    this.leftIndex = leftIndex;
+    this.rightIndex = rightIndex;
     open();
   }
 
   @Override
   public boolean hasNext() {
-    if (stash == null) return false;
-    if (leftIndex == null || rightIndex == null) {
-      close();
-      return false;
-    }
+    if (stash == null) return doesNotHaveNext();
     if (next != null) return true;
-    if ((!leftIterator.hasNext() && left == null) ||
-        (!rightIterator.hasNext() && right == null && stash.isEmpty())) {
-      close();
-      return false;
-    }
-    
-    if (left == null) left = leftIterator.next();
-    if (right == null) right = rightIterator.next();
-    boolean alt = false;
-    if (right == null) {
-      left = leftIterator.next();
-      if (left == null) {
-        close();
-        return false;
-      }
+    if (left == null) {
       stashIndex = 0;
-      alt = true;
-      right = stash.get(stashIndex);
+      left = leftIterator.next();
     }
+    if (left == null) return doesNotHaveNext();
+    if (right == null) right = rightIterator.next();
+    if (right == null && stashIndex >= stash.size()) return doesNotHaveNext();
     
+    // Acquire the correct values
     LeafValue leftValue = left.getValue(leftIndex);
-    LeafValue rightValue = right.getValue(rightIndex);
-//    System.out.println(new Row(left, right));
-//    System.out.println(stash);
+    LeafValue rightValue = right == null ? null : right.getValue(rightIndex);
     
+    // Set the comparison type if it wasn't already
     if (type == NONE) {
       if (leftValue instanceof LongValue && rightValue instanceof LongValue) type = LONG;
       else if (leftValue instanceof DoubleValue && rightValue instanceof DoubleValue) type = DOUBLE;
@@ -115,47 +64,49 @@ public class SortJoinIterator implements RowIterator {
       else type = MISMATCH;
     }
     
-    if (type == MISMATCH) {
-      close();
-      return false;
-    }
-    
-    Integer compare = compareValues(leftValue, rightValue);
-    if (alt && compare == 0) compare = -1; 
-    
-    if (compare == null) {
-      close();
-      return false;
-    } else if (compare == 0) {
-      if (stash.isEmpty()) {
-        stashIndex = 0;
+    if (right == null) {
+      // Finish up all rows on the left
+      Row stashed = stash.get(stashIndex);
+      Integer compare = compareValues(leftValue, stashed.getValue(rightIndex));
+      if (compare == 0) {
+        next = new Row(left, stashed);
+        return true;
       } else {
-        if (compareValues(left.getValue(leftIndex), stash.get(0).getValue(rightIndex)) != 0) {
-          stashIndex = 0;
-          stash.clear();
-        }
+        return doesNotHaveNext();
       }
-      stash.add(right);
-      right = null;
-      next = new Row(left, stash.get(stashIndex));
-      return true;
-    } else if (compare < 0) {
-      if (stash.isEmpty()) {
-        left = null;
-        return this.hasNext();
-      } else {
+    } else {
+      Integer compare = compareValues(leftValue, rightValue);
+      // Proceed as normal
+      if (compare == 0) {
+        // left == right
+//        if (!stash.isEmpty()) {
+//          LeafValue stashedValue = stash.get(0).getValue(rightIndex);
+//          if (compareValues(leftValue, stashedValue) != 0) {
+//            stash.clear();
+//            stashIndex = 0;
+//          }
+//        }
+//        
+        next = new Row(left, right);
+        stash.add(right);
+        right = null;
+        return true;
+      } else if (compare < 0) {
+        // left < right
         if (stashIndex >= stash.size()) {
-          stashIndex = 0;
           left = null;
           return this.hasNext();
         } else {
           next = new Row(left, stash.get(stashIndex));
           return true;
         }
+      } else if (compare > 0) {
+        // left > right
+        right = null;
+        return this.hasNext();
+      } else {
+        return doesNotHaveNext();
       }
-    } else {
-      right = null;
-      return this.hasNext();
     }
   }
   
@@ -189,6 +140,16 @@ public class SortJoinIterator implements RowIterator {
       return null;
     }
   }
+  
+  /**
+   * Helper function for hasNext(), closes the iterator and returns false.
+   * 
+   * @return false
+   */
+  private boolean doesNotHaveNext() {
+    close();
+    return false;
+  }
 
   @Override
   public Row next() {
@@ -202,20 +163,22 @@ public class SortJoinIterator implements RowIterator {
   @Override
   public void close() {
     if (stash == null) return;
-    leftIterator.close();
-    rightIterator.close();
+    stash = null;
+    stashIndex = 0;
+    type = NONE;
     left = null;
     right = null;
     next = null;
-    stash = null;
-    type = NONE;
   }
 
   @Override
   public void open() {
     if (stash != null) return;
-    leftIterator.open();
-    rightIterator.open();
     stash = new ArrayList<Row>();
+    stashIndex = 0;
+    type = NONE;
+    left = null;
+    right = null;
+    next = null;
   }
 }
--- src/edu/buffalo/cse562/parsetree/JoinNode.java
+++ src/edu/buffalo/cse562/parsetree/JoinNode.java
@@ -1,11 +1,17 @@
 package edu.buffalo.cse562.parsetree;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import net.sf.jsqlparser.expression.BinaryExpression;
 import net.sf.jsqlparser.expression.Expression;
-import edu.buffalo.cse562.iterator.CartesianIterator;
+import net.sf.jsqlparser.expression.Parenthesis;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.select.OrderByElement;
+import edu.buffalo.cse562.iterator.MergeSortIterator;
 import edu.buffalo.cse562.iterator.RowIterator;
-import edu.buffalo.cse562.iterator.SelectIterator;
+import edu.buffalo.cse562.iterator.SortJoinIterator;
 import edu.buffalo.cse562.table.Row;
 import edu.buffalo.cse562.table.Schema;
 
@@ -16,9 +22,12 @@ import edu.buffalo.cse562.table.Schema;
  * @author Sunny Mistry
  */
 public class JoinNode extends ParseTree {
-  private final Expression expression;
   private final Schema outSchema;
-  
+  private final int    leftIndex;
+  private final int    rightIndex;
+  private final List<OrderByElement> leftOrders;
+  private final List<OrderByElement> rightOrders;
+
   /**
    * Initializes the Join node.
    * 
@@ -31,16 +40,53 @@ public class JoinNode extends ParseTree {
     super(base);
     super.left = left;
     super.right = right;
-    this.expression = expression;
+    
+    // Set the output schema
     outSchema = new Schema(left.getSchema(), right.getSchema());
+    
+    // Unwrap the expression
+    while (expression instanceof Parenthesis)
+      expression = ((Parenthesis) expression).getExpression();
+    BinaryExpression binaryExpression = (BinaryExpression) expression;
+    
+    // Get the join columns
+    Column columnOne = (Column) binaryExpression.getLeftExpression();
+    Column columnTwo = (Column) binaryExpression.getRightExpression();
+    
+    // Initialize the order as ascending
+    OrderByElement leftOrder = new OrderByElement();
+    leftOrder.setAsc(true);
+    OrderByElement rightOrder = new OrderByElement();
+    rightOrder.setAsc(true);
+    
+    // Initialize the correct left and right columns and their index
+    Integer index = left.getSchema().getIndex(columnOne);
+    if (index == null) {
+      leftIndex = left.getSchema().getIndex(columnTwo);
+      leftOrder.setExpression(columnTwo);
+      rightIndex = right.getSchema().getIndex(columnOne);
+      rightOrder.setExpression(columnOne);
+    } else {
+      leftIndex = index;
+      leftOrder.setExpression(columnOne);
+      rightIndex = right.getSchema().getIndex(columnTwo);
+      rightOrder.setExpression(columnTwo);
+    }
+    
+    // Initialize the order rules
+    leftOrders = new ArrayList<OrderByElement>(1);
+    rightOrders = new ArrayList<OrderByElement>(1);
+    leftOrders.add(leftOrder);
+    rightOrders.add(rightOrder);
   }
 
   @Override
   public Iterator<Row> iterator() {
     RowIterator leftIterator = (RowIterator) left.iterator();
     RowIterator rightIterator = (RowIterator) right.iterator();
-    CartesianIterator cartesianIterator = new CartesianIterator(leftIterator, rightIterator);
-    return new SelectIterator(cartesianIterator, expression, outSchema);
+    leftIterator = new MergeSortIterator(leftIterator, leftOrders, left.getSchema());
+    rightIterator = new MergeSortIterator(rightIterator, rightOrders, right.getSchema());
+    return new SortJoinIterator(leftIterator, rightIterator, leftIndex, rightIndex);
   }
 
   @Override
