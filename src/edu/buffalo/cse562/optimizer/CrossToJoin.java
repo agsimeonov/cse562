eb369e9f4d2b007f4c1d93ffed769749142adb85
Alexander Simeonov <agsimeon@buffalo.edu>
2015-03-23 14:55:06 -0400

    Progress on converting cartesian to join.

--- src/edu/buffalo/cse562/optimizer/CrossToJoin.java
+++ src/edu/buffalo/cse562/optimizer/CrossToJoin.java
@@ -1,7 +1,32 @@
 package edu.buffalo.cse562.optimizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.buffalo.cse562.parsetree.CartesianNode;
+import edu.buffalo.cse562.parsetree.ParseTree;
+
 public class CrossToJoin {
-  public CrossToJoin() {
+  // get all cross
+  // if select above cross (in sequence) has expression instanceof equals to
+  // convert to join
+  
+  public static void convert(ParseTree node) {
+    if (!(node instanceof CartesianNode)) return;
+    if (node.getBase() == null) return;
+    ParseTree parent = node.getBase();
     
+  }
+  
+  public static List<CartesianNode> getAllCartesianNodes(ParseTree root) {
+    List<CartesianNode> list = new ArrayList<CartesianNode>();
+    
+    if (root != null) {
+      if (root instanceof CartesianNode) list.add((CartesianNode) root);
+      if (root.getLeft() != null) list.addAll(getAllCartesianNodes(root.getLeft()));
+      if (root.getRight() != null) list.addAll(getAllCartesianNodes(root.getRight()));
+    }
+    
+    return list;
   }
 }
