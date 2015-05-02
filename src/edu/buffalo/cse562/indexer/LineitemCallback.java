14999ede43c2384a523597573216c71653b9d48c
Alexander Simeonov <agsimeon@buffalo.edu>
2015-05-02 04:45:30 -0400

    Improvements to indexer callbacks.

--- src/edu/buffalo/cse562/indexer/LineitemCallback.java
+++ src/edu/buffalo/cse562/indexer/LineitemCallback.java
@@ -12,6 +12,7 @@ import edu.buffalo.cse562.table.TableManager;
 public class LineitemCallback implements Callback {
   private final int lineitem_orderkey;
   private final int lineitem_shipdate;
+  private final int lineitem_returnflag;
   private final int mode;
   private Set<Long> keySet;
   private long      threshold;
@@ -21,6 +22,7 @@ public class LineitemCallback implements Callback {
     keySet = IndexManager.getDatabase(IndexManager.ORDERS, "orders.orderkey", ordersFile).keySet();
     lineitem_orderkey = dataTable.getSchema().getLookupTable().get("lineitem.orderkey");
     lineitem_shipdate = dataTable.getSchema().getLookupTable().get("lineitem.shipdate");
+    lineitem_returnflag = dataTable.getSchema().getLookupTable().get("lineitem.returnflag");
     if (mode == 3) threshold = new DateValue("'1995-02-15'").getValue().getTime();
     this.mode = mode;
   }
@@ -31,6 +33,10 @@ public class LineitemCallback implements Callback {
       if (mode == 3) {
         long time = ((DateValue) row.getValue(lineitem_shipdate)).getValue().getTime();
         return (time <= threshold) ||!keySet.contains(row.getValue(lineitem_orderkey).toLong());
+      }
+      if (mode == 10) {
+        String flag = row.getValue(lineitem_returnflag).toString();
+        if (flag.equals("'R'")) return true;
       }
       return !keySet.contains(row.getValue(lineitem_orderkey).toLong());
     } catch (InvalidLeaf e) {
