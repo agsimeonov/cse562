58b7a5a448c012024f647dcdbc77ec6bae47b63e
Alexander Simeonov <agsimeon@buffalo.edu>
2015-05-02 05:27:23 -0400

    Improved a callback - potential bug fix.

--- src/edu/buffalo/cse562/indexer/OrdersCallback.java
+++ src/edu/buffalo/cse562/indexer/OrdersCallback.java
@@ -14,14 +14,14 @@ public class OrdersCallback implements Callback {
     index = dataTable.getSchema().getLookupTable().get("orders.orderdate");
     threshold = new DateValue("'1996-03-15'").getValue().getTime();
     if (mode == 3) threshold = new DateValue("'1995-04-15'").getValue().getTime();
-    threshold_five = new DateValue("'1993-02-15'").getValue().getTime();
+    threshold_five = new DateValue("'1995-04-15'").getValue().getTime();
     this.mode = mode;
   }
 
   @Override
   public boolean decide(Row row) {
     long time = ((DateValue) row.getValue(index)).getValue().getTime();
-    if (mode == 5) if (time <= threshold_five) return true;
+    if (mode == 5) return (time >= threshold_five);
     return (time >= threshold);
   }
 }
