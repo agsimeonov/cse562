2c5896daf17af6ae6584101337e61292b3479c07
Alexander Simeonov <agsimeon@buffalo.edu>
2015-05-02 03:14:03 -0400

    More progress towards Indexer.

--- src/edu/buffalo/cse562/indexer/OrdersCallback.java
+++ src/edu/buffalo/cse562/indexer/OrdersCallback.java
@@ -14,14 +14,14 @@ public class OrdersCallback implements Callback {
     index = dataTable.getSchema().getLookupTable().get("orders.orderdate");
     threshold = new DateValue("'1996-03-15'").getValue().getTime();
     if (mode == 3) threshold = new DateValue("'1995-04-15'").getValue().getTime();
-    threshold_ten = new DateValue("'1994-03-15'").getValue().getTime();
+//    threshold_ten = new DateValue("'1994-03-15'").getValue().getTime();
     this.mode = mode;
   }
 
   @Override
   public boolean decide(Row row) {
     long time = ((DateValue) row.getValue(index)).getValue().getTime();
-    if (mode == 10 || mode == 5) return (time >= threshold) || (time <= threshold_ten);
+//    if (mode == 10 || mode == 5) return (time >= threshold) || (time <= threshold_ten);
     return (time >= threshold);
   }
 }
