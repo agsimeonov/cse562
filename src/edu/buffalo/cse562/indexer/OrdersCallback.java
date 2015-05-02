e21b0226a834c3787d30b7bf654d6502c910fb30
Alexander Simeonov <agsimeon@buffalo.edu>
2015-05-02 05:02:09 -0400

    Further improved callbacks.

--- src/edu/buffalo/cse562/indexer/OrdersCallback.java
+++ src/edu/buffalo/cse562/indexer/OrdersCallback.java
@@ -8,20 +8,20 @@ public class OrdersCallback implements Callback {
   private final int index;
   private final int mode;
   private long      threshold;
-  private long      threshold_ten;
+  private long      threshold_five;
   
   public OrdersCallback(DataTable dataTable, int mode) {
     index = dataTable.getSchema().getLookupTable().get("orders.orderdate");
     threshold = new DateValue("'1996-03-15'").getValue().getTime();
     if (mode == 3) threshold = new DateValue("'1995-04-15'").getValue().getTime();
-//    threshold_ten = new DateValue("'1994-03-15'").getValue().getTime();
+    threshold_five = new DateValue("'1994-02-15'").getValue().getTime();
     this.mode = mode;
   }
 
   @Override
   public boolean decide(Row row) {
     long time = ((DateValue) row.getValue(index)).getValue().getTime();
-//    if (mode == 10 || mode == 5) return (time >= threshold) || (time <= threshold_ten);
+    if (mode == 5) if (time <= threshold_five) return true;
     return (time >= threshold);
   }
 }
