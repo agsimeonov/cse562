de4fee7346283d7ba0e148c9571688e2fee36a86
Alexander Simeonov <agsimeon@buffalo.edu>
2015-05-02 16:01:33 -0400

    Improved a callback - potential bug fix - progress towards better value results.

--- src/edu/buffalo/cse562/indexer/OrdersCallback.java
+++ src/edu/buffalo/cse562/indexer/OrdersCallback.java
@@ -16,7 +16,7 @@ public class OrdersCallback implements Callback {
     threshold = new DateValue("'1996-03-15'").getValue().getTime();
     if (mode == 3) threshold = new DateValue("'1995-04-15'").getValue().getTime();
     threshold_five = new DateValue("'1995-03-15'").getValue().getTime();
-    threshold_five_lower = new DateValue("'1993-05-30'").getValue().getTime();
+    threshold_five_lower = new DateValue("'1993-03-15'").getValue().getTime();
     this.mode = mode;
   }
 
